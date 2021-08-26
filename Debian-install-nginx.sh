ab5755cada3d1c769d095ee84be40e720d18adc8
zunsking <36225896+zunsking@users.noreply.github.com>
2021-08-26 11:38:39 +0800

    Update Debian-install-nginx.sh

--- Debian-install-nginx.sh
+++ Debian-install-nginx.sh
@@ -27,7 +27,7 @@ mkdir /etc/nginx/vhost -p
 #添加虚拟主机支持
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 wget -q -O nginx.conf https://raw.githubusercontent.com/zunsking/Debian-Install-Nginx/master/nginx.conf
-\cp nginx.conf /etc/nginx/
+mv nginx.conf /etc/nginx/
 #注册系统服务
 cat >/lib/systemd/system/nginx.service << EOF
 [Unit]
